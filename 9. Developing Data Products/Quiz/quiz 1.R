cd59f931d07a7ed0125e7c7106c153697bdb0c04
Yohannbos <yohann.bosco@gmail.com>
2016-04-21 13:32:05 +0200

    9th Course

---
+++ 9. Developing Data Products/Quiz/quiz 1.R
@@ -0,0 +1,42 @@
+#Question 1
+library(manipulate)
+myPlot <- function(s) {
+  plot(cars$dist - mean(cars$dist), cars$speed - mean(cars$speed))
+  abline(0, s)
+}
+
+manipulate(myPlot(s), s = slider(0, 2, step = 0.1))
+
+#question 2
+
+require(devtools)
+install_github('rCharts', 'ramnathv')
+data("airquality")
+dTable(airquality, sPaginationType = "full_numbers")
+
+#Question 4
+library(shiny)
+
+shinyUI(pageWithSidebar(
+  headerPanel("Example plot"),
+  sidebarPanel(
+    sliderInput('mu', 'Guess at the mu',value = 70, min = 60, max = 80, step = 0.05,)  ),
+  mainPanel(
+    plotOutput('myHist')
+  )
+))
+
+library(UsingR)
+data(galton)
+
+shinyServer(
+  function(input, output) {
+    output$myHist <- renderPlot({
+      hist(galton$child, xlab='child height', col='lightblue',main='Histogram')
+      mu <- input$mu
+      lines(c(mu, mu), c(0, 200),col="red",lwd=5)
+      mse <- mean((galton$child - mu)^2)
+      text(63, 150, paste("mu = ", mu))
+      text(63, 140, paste("MSE = ", round(mse, 2)))
+    })      }
+)
